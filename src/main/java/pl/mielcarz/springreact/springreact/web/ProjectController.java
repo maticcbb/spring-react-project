0fb689fd26f8ce7618f7b2775cc04c72d9b16c08
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-06-17 19:16:22 +0200

    Error and bad request handling

--- src/main/java/pl/mielcarz/springreact/springreact/web/ProjectController.java
+++ src/main/java/pl/mielcarz/springreact/springreact/web/ProjectController.java
@@ -3,12 +3,15 @@ package pl.mielcarz.springreact.springreact.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.mielcarz.springreact.springreact.domain.Project;
 import pl.mielcarz.springreact.springreact.services.ProjectService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/project")
@@ -18,7 +21,11 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
     }
