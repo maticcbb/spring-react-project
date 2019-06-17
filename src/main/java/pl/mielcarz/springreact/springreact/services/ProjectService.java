673c7f0c36099393827dade4d199ae85cdd118f6
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-06-17 15:18:45 +0200

    Service and controller

---
+++ src/main/java/pl/mielcarz/springreact/springreact/services/ProjectService.java
@@ -0,0 +1,18 @@
+package pl.mielcarz.springreact.springreact.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.mielcarz.springreact.springreact.domain.Project;
+import pl.mielcarz.springreact.springreact.domain.ProjectRepository;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        return projectRepository.save(project);
+    }
+
+}
