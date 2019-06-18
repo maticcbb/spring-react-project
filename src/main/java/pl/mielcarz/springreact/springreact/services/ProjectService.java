f750660b069367ee32099817f64ab702a78c9bd6
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-06-18 18:01:07 +0200

    Custom Exception for unique project identifiers

--- src/main/java/pl/mielcarz/springreact/springreact/services/ProjectService.java
+++ src/main/java/pl/mielcarz/springreact/springreact/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.mielcarz.springreact.springreact.domain.Project;
 import pl.mielcarz.springreact.springreact.domain.ProjectRepository;
+import pl.mielcarz.springreact.springreact.exceptions.ProjectIdException;
 
 @Service
 public class ProjectService {
@@ -11,8 +12,13 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exist");
+        }
     }
 
 }
